0ac488c2ef3229308dee1d7395acf45a29661fbb
Tray Torrance <torrancew@gmail.com>
2025-03-08 13:37:24 -0800

    Adjust Grid Import and Export as well

--- custom_components/franklin_wh/sensor.py
+++ custom_components/franklin_wh/sensor.py
@@ -165,7 +165,7 @@ class GridImportSensor(SensorEntity):
     _attr_name = "FranklinWH Grid Import"
     _attr_native_unit_of_measurement = UnitOfEnergy.KILO_WATT_HOUR
     _attr_device_class = SensorDeviceClass.ENERGY
-    _attr_state_class = SensorStateClass.TOTAL
+    _attr_state_class = SensorStateClass.MEASUREMENT
 
     def __init__(self, cache):
         self._cache = cache
@@ -184,7 +184,7 @@ class GridExportSensor(SensorEntity):
     _attr_name = "FranklinWH Grid Export"
     _attr_native_unit_of_measurement = UnitOfEnergy.KILO_WATT_HOUR
     _attr_device_class = SensorDeviceClass.ENERGY
-    _attr_state_class = SensorStateClass.TOTAL
+    _attr_state_class = SensorStateClass.MEASUREMENT
 
     def __init__(self, cache):
         self._cache = cache
